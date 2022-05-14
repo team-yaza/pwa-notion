945b3a3d7832a29a842c5dc8495fa3960a0ab4e7
hyunjin <leehj0110@kakao.com>
2022-05-14 19:25:27 +0900

    feat: babel 설정 (next/babel)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pwa-notion
-노션을 비슷하게 만들어 보면서 공부합니다. 
+
+노션을 비슷하게 만들어 보면서 공부합니다.
+
+[Component Driven Development (CDD)](https://hyunjinee.tistory.com/75)를 활용해서 개발합니다.
--- frontend/components/postEdit/style.ts
+++ frontend/components/postEdit/style.ts
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   height: 100%;
-  background-color: blue;
+  /* background-color: blue; */
   font-size: 5rem;
 `;
